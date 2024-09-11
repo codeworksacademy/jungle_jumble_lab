6242cc47cb3a08c6daaee0fc5ea0111b9b0aece1
Jeremy-Fowler <jembyfowler@gmail.com>
2024-09-11 14:28:04 -0600

    drawing jumbles

--- app/controllers/JumblesController.js
+++ app/controllers/JumblesController.js
@@ -1,5 +1,16 @@
+import { AppState } from "../AppState.js";
+import { setHTML } from "../utils/Writer.js";
+
 export class JumblesController {
   constructor() {
     console.log('Jumbles Controller Loaded 🐒');
+    this.drawJumbles()
+  }
+
+  drawJumbles() {
+    const jumbles = AppState.jumbles
+    let jumblesHTML = ''
+    jumbles.forEach(jumble => jumblesHTML += jumble.listHTMLTemplate)
+    setHTML('jumbles-list', jumblesHTML)
   }
 }
--- app/models/Jumble.js
+++ app/models/Jumble.js
@@ -14,7 +14,7 @@ export class Jumble {
     return `
     <div class="d-flex justify-content-between align-items-center mb-2">
       <button class="btn btn-warning fw-bold" type="button">start</button>
-      <b>${this.name} Jumble</b>
+      <b>${this.name}</b>
       <span class="fw-bold">⏲️ 12.2s</span>
       <span class="fw-bold">55.2 wpm</span>
     </div>
